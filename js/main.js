3620af120fb26d7647f103a21760cf84c91e88a3
smontoro <sarahjmontoro@gmail.com>
2017-11-18 21:32:43 -0800

    added am and pm loop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-function checkTime(i) {
-	if (i < 10) {
-		i = "0" + i
-	}
-
-	return i 
-};
-
 
 function showTime() {
 
@@ -13,14 +5,20 @@ function showTime() {
 	var hours = date.getHours()
 	var minutes = date.getMinutes()
 	var seconds = date.getSeconds()
+		seconds.className = "ampm"
 
 	//this needs to run in the document before it is printed in the text node
-	if (hours > 12) {
-		hours = hours - 12
-	}
 
 	minutes = checkTime(minutes)
     seconds = checkTime(seconds)
+
+	if (hours < 12) {
+		seconds = seconds + " a.m."
+	} else if (hours >= 12) {
+		hours = hours - 12 
+		seconds = seconds + " p.m."
+	}
+
 
 	document.getElementById("main").textContent = hours + ":" + minutes + ":" + seconds
 
@@ -32,3 +30,10 @@ function showTime() {
 
 };
 
+function checkTime(i) {
+	if (i < 10) {
+		i = "0" + i
+	}
+
+	return i 
+};
